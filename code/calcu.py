6b78ad5ab8d3ee5cf6b4c6a7a466b91081711f6e
alejobr23 <alejobuitrago0216@gmail.com>
2026-09-17 20:20:39 -0500

    cambios en el archivo calcu.py con operaciones

---
+++ code/calcu.py
@@ -0,0 +1,17 @@
+def suma(valor1, valor2):
+    return valor1 + valor2
+
+def resta(valor1, valor2):
+    return valor1 - valor2
+
+def multi(valor1, valor2):
+    return valor1 * valor2
+
+resulado_suma = suma(5, 2)
+resulado_resta = resta(5, 2)
+resulado_multi = multi(5, 2)
+
+print (f"Resultado suma: {resulado_suma}")
+print (f"Resultado resta: {resulado_resta}")
+print (f"Resultado multi: {resulado_multi}")
+
