70842575d57fc9579a4380502a0721394f289183
alejobr23 <alejobuitrago0216@gmail.com>
2026-09-22 20:56:32 -0500

    Se sube trabajo de concu en clase

---
+++ code/concurrente.py
@@ -0,0 +1,22 @@
+import multiprocessing, time
+
+def print_numbers():
+    for i in range(1, 6):
+        print (f"numeros: {i}")
+        time.sleep (1)
+
+def print_letter():
+    for letter in "ABCDE":
+        print (f"letter: {letter}")
+        time.sleep(1)
+
+
+if __name__ == "__main__" :
+    p1 = multiprocessing.Process(target=print_numbers)
+    p2 = multiprocessing.Process(target=print_letter)
+
+    p1.start()
+    p2.start()
+
+    p1.join()
+    p2.join()
